ba6cb837f7f6fc7dc180490a36558c358b559892
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-02-29 23:57:14 +0900

    구글 로그인 추가

--- src/main/java/com/hoyoung/springboot/web/IndexController.java
+++ src/main/java/com/hoyoung/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.hoyoung.springboot.web;
 
+import com.hoyoung.springboot.config.auth.LoginUser;
+import com.hoyoung.springboot.config.auth.dto.SessionUser;
+import com.hoyoung.springboot.domain.user.UserRepository;
 import com.hoyoung.springboot.service.PostsService;
 import com.hoyoung.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         // src/main/resources/templates/index.mustache
         return "index";
     }
